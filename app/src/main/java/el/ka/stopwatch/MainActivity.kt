25d8c69a773c4774ada05d78d25030e37a49bf72
Ekaterina-El <hackerrussia@yandex.ru>
2021-11-25 16:17:57 +0500

    Done

--- app/src/main/java/el/ka/stopwatch/MainActivity.kt
+++ app/src/main/java/el/ka/stopwatch/MainActivity.kt
@@ -1,11 +1,62 @@
 package el.ka.stopwatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var sec = 0
+    var isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startTimer()
+
+        btn_start.setOnClickListener {
+            isRunning = true
+            it.visibility = View.GONE
+            btn_pause.visibility = View.VISIBLE
+            btn_stop.visibility = View.VISIBLE
+        }
+
+        btn_pause.setOnClickListener {
+            isRunning = !isRunning
+            sec -= 1
+            btn_pause.text = if (isRunning) getString(R.string.pause) else getString(R.string.start)
+        }
+
+        btn_stop.setOnClickListener {
+            isRunning = false
+            sec = 0
+
+            btn_stop.visibility = View.GONE
+            btn_pause.visibility = View.GONE
+            btn_start.visibility = View.VISIBLE
+        }
+    }
+
+    private fun startTimer() {
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val hrs = sec.div(3600)
+                val min = (sec.mod(3600)).div(60)
+                val secs = sec.mod(60)
+
+                val timeT = String.format("%d:%02d:%02d", hrs, min, secs)
+
+                if (isRunning) {
+                    sec += 1
+                }
+
+                tv_time_value.text = timeT
+
+                handler.postDelayed(this, 1000)
+            }
+
+        })
     }
 }
